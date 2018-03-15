f28afc900d37360c5651ff7c63b2ad7cfbef4284
sawim <mwasowicz7@gmail.com>
2018-03-15 21:39:23 +0100

    changed user's database types

--- config/models/user.js
+++ config/models/user.js
@@ -1,12 +1,12 @@
 const Symbol = require('sequelize');
 
 module.exports = {
-    name: Symbol.STRING,
-    lastName: Symbol.STRING,
-    email: Symbol.STRING,
+    name: Symbol.STRING(20),
+    lastName: Symbol.STRING(20),
+    email: Symbol.STRING(60),
     profileId: {    //facebook profile id
-        type: Symbol.STRING,
+        type: Symbol.STRING(100),
         unique: true
     },
-    accessToken: Symbol.STRING   // access token received from facebook
+    accessToken: Symbol.TEXT   // access token received from facebook
 };
