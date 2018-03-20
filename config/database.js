de51893ca7a6b7c832046e462d9af5b690e4ae02
Matshec <mmasakra@gmail.com>
2018-03-20 22:16:37 +0100

    init done

--- config/database.js
+++ config/database.js
@@ -1,13 +1,23 @@
 const Sequelize = require('sequelize');
 const user = require('./models/user');
+const users_timetable = require('./models/users_timetable');
 
+//just to debug - forces to drop and recreate tables
+forceNew = true;
 
 const db = new Sequelize(process.env.DATABASE_URL);
 
 const User = db.define('users', user);
+User.sync({force: forceNew});
 
-User.sync();
+const UsersTimetable = db.define('users_timetables',users_timetable,);
+UsersTimetable.sync({force: forceNew});
+
+User.hasOne(UsersTimetable);
+
+
 
 module.exports = {
-    User
+    User,
+    UsersTimetable
 };
