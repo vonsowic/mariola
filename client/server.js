2724bce8788fad9a0e3417e277b7867c705bb4e0
sawim <mwasowicz7@gmail.com>
2018-03-26 18:09:22 +0200

    -

--- client/server.js
+++ client/server.js
@@ -4,13 +4,13 @@ const express = require('express');
 
 const bundler = new Bundler('./public/index.html');
 const app = express();
-
+let p = `${process.env.URL || 'http://localhost'}:${process.env.API_PORT || 5000}`
 app.use('/api',
     proxy({
-        target: `${process.env.URL || 'http://localhost'}:${process.env.API_PORT || 5000}`
+        target: p
     })
 );
 
 app.use(bundler.middleware());
-
+console.log('proxy set to ' + p);
 app.listen(Number(process.env.PORT || 3000));
