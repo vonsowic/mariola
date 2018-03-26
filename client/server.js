75d77f99f160b2cc5094dc8ec2f73b30a7082df6
sawim <mwasowicz7@gmail.com>
2018-03-26 17:40:44 +0200

    -

--- client/server.js
+++ client/server.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 const Bundler = require('parcel-bundler');
 const express = require('express');
 
-const bundler = new Bundler('./local/index.html');
+const bundler = new Bundler('./public/index.html');
 const app = express();
 
 app.use('/api',
