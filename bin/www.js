569108dffbb1eee94c70c7b809f7f9d0ffa74db1
sawim <mwasowicz7@gmail.com>
2018-04-15 21:58:16 +0200

    notification service

--- bin/www.js
+++ bin/www.js
@@ -2,5 +2,7 @@ require('dotenv').config();
 require('database').connection.sync({force: process.env.DROP_DATABASE});
 
 const app = require('../app');
+const notificationService = require('notification-service');
 
 app.listen(process.env.API_PORT || 5000);
+notificationService(process.env.NOTIFICATION_PORT || 5001);
