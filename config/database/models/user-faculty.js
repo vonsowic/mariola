7bfe36bd876ab3819cef4353723b0b7bae931206
sawim <mwasowicz7@gmail.com>
2018-07-05 10:28:40 +0200

    create groupd field add required modifier to all fields

--- config/database/models/user-faculty.js
+++ config/database/models/user-faculty.js
@@ -3,10 +3,16 @@ const Symbol = require('sequelize');
 module.exports={
     isAdmin: {
         type: Symbol.BOOLEAN,
-        defaultValue: false
+        defaultValue: false,
+        required: true
     },
     isBanned: {
         type: Symbol.BOOLEAN,
-        defaultValue: false
+        defaultValue: false,
+        required: true
+    },
+    group: {
+        type: Symbol.String(2),
+        required: true
     }
 };
