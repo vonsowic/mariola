d9a84f495c3aec9267b24bf362917ff0d6af3684
sawim <mwasowicz7@gmail.com>
2018-07-29 20:22:53 +0200

    store salt in redis cache

--- config/passport-mariola/salt.js
+++ config/passport-mariola/salt.js
@@ -1,6 +1,27 @@
 const generateSalt = require('random-token');
+const cache = require('@cache');
+
+let salt, refreshSalt;
+
+cache.getAsync('salt:token')
+    .then(res => {
+        salt = res
+    });
+
+cache.getAsync('salt:refreshToken')
+    .then(res => {
+        refreshSalt = res
+    });
+
+if(!salt || !refreshSalt) {
+    salt = generateSalt(16);
+    cache.set('salt:token', salt);
+
+    refreshSalt = generateSalt(64);
+    cache.set('salt:refreshToken', refreshSalt);
+}
 
 module.exports={
-    salt: generateSalt(16),
-    refreshSalt: generateSalt(64)
+    salt,
+    refreshSalt
 };
