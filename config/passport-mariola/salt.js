a9a0091aee9ce54f0880172e05e0bcd2ad887ccb
sawim <mwasowicz7@gmail.com>
2018-07-30 09:36:40 +0200

    fix loading salts from redis

--- config/passport-mariola/salt.js
+++ config/passport-mariola/salt.js
@@ -5,23 +5,25 @@ let salt, refreshSalt;
 
 cache.getAsync('salt:token')
     .then(res => {
-        salt = res
+        salt = res;
+        return cache.getAsync('salt:refreshToken')
+            .then(res => {
+                refreshSalt = res;
+            })
+    })
+    .catch(() => {
+        salt = generateSalt(16);
+        cache.set('salt:token', salt);
+
+        refreshSalt = generateSalt(64);
+        cache.set('salt:refreshToken', refreshSalt);
+    })
+    .finally(() => {
+        module.exports={
+            salt,
+            refreshSalt
+        };
     });
 
-cache.getAsync('salt:refreshToken')
-    .then(res => {
-        refreshSalt = res
-    });
 
-if(!salt || !refreshSalt) {
-    salt = generateSalt(16);
-    cache.set('salt:token', salt);
 
-    refreshSalt = generateSalt(64);
-    cache.set('salt:refreshToken', refreshSalt);
-}
-
-module.exports={
-    salt,
-    refreshSalt
-};
